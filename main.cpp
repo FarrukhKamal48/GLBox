f2e40362fd4d5ecf1c1a3f3bc549d8059a43a725
FarrukhKamal48 <farrukhkamal429@gmail.com>
2024-02-03 13:01:18 +0500

    New scene for verlet physics

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include "src/scenes/SceneCircle.h"
+#include "src/scenes/SceneVerlet.h"
 
 int main (int argc, char *argv[])
 {
@@ -28,14 +28,15 @@ int main (int argc, char *argv[])
 
     glewInit();
 
-    Scene::Circle TriangleScene;
+    Scene::Verlet VerletScene;
 
        /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         
-        TriangleScene.Render();
+        VerletScene.Update(0);
+        VerletScene.Render();
         
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
