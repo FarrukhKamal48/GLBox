fa1dd177541b2189fd908c74e98ddc96755a64e6
FarrukhKamal48 <farrukhkamal429@gmail.com>
2024-02-09 19:25:02 +0500

    circle primative now uses rectangles (to simplify calculations)

--- src/primatives/PrimativeCircle.h
+++ src/primatives/PrimativeCircle.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include <memory>
 #include "../Renderer.h"
 #include "../Texture.h"
@@ -17,35 +18,80 @@ private:
 
     glm::vec2 m_Centre;
     float m_Radius;
-    
-    glm::mat4 m_Proj;
-    glm::mat4 m_View;
     glm::mat4 m_Model;
-    glm::mat4 m_MVP;
+    
 public:
-    Circle(glm::mat4 proj, glm::mat4 view, glm::vec2 centre, float radius) :
+    Circle() :
+        m_Centre(glm::vec2(WIDTH/2, HEIGHT/2)),
+        m_Radius(50.0f),
+        m_Model(glm::translate(glm::mat4(1.0f), glm::vec3(m_Centre, 0.0f)))
+    { 
+        Init(); 
+    }
+    
+    Circle(glm::vec2 centre, float radius) :
         m_Centre(centre),
         m_Radius(radius),
-        m_Proj(proj),
-        m_View(view),
-        m_Model(glm::translate(glm::mat4(1.0f), glm::vec3(centre, 0.0f))),
-        m_MVP(m_Proj * m_View * m_Model)
+        m_Model(glm::translate(glm::mat4(1.0f), glm::vec3(centre, 0.0f)))
     { 
+        Init(); 
+    }
+    
+    Circle(const Circle& other) :
+        m_Centre(other.m_Centre),
+        m_Radius(other.m_Radius),
+        m_Model(other.m_Model)
+    { 
+        Init(); 
+    }
+    ~Circle() { }
+
+    void SetRadius(float radius) {
+        m_Model[0][0] *= radius/m_Radius;
+        m_Model[1][1] *= radius/m_Radius;
+        m_Radius = radius;
+        shader->Bind();
+        shader->SetUniform("u_EdgeSmooth", 1.2f/m_Radius);
+    }
+
+    void SetCentre(glm::vec2 position) {
+        m_Model[3][0] = position.x;
+        m_Model[3][1] = position.y;
+        m_Centre = position;
+    }
+    
+    void SetColor(float r, float g, float b, float a) {
+        shader->Bind();
+        shader->SetUniformVec4("u_Color", r, g, b, a);
+    }
+
+    void Draw(glm::mat4& Proj, glm::mat4 View) {
+        shader->Bind();
+        shader->SetUniformMat4("u_MVP", Proj * View * m_Model);
+        
+        Renderer renderer;
+        renderer.Draw(*vertexArray, *indexBuffer, *shader);
+    }
+    
+private:
+    void Init() {
         float positions[] = {
             -m_Radius, -m_Radius, 0.0f, 0.0f,
              m_Radius, -m_Radius, 1.0f, 0.0f,
-             0.0f                 ,  m_Radius, 0.5f, 1.0f,
+             m_Radius,  m_Radius, 1.0f, 1.0f,
+            -m_Radius,  m_Radius, 0.0f, 1.0f,
         };
         unsigned int indices[] = {
             0, 1, 2, 
+            2, 3, 0
         };
         
         Renderer renderer;
         renderer.BasicBlend();
 
         vertexArray = std::make_unique<VertexArray>();
-        m_VertexBuffer = std::make_unique<VertexBuffer>(positions, 3 * 4 * sizeof(float));
-        indexBuffer =  std::make_unique<IndexBuffer>(indices, 3);
+        m_VertexBuffer = std::make_unique<VertexBuffer>(positions, 4 * 4 * sizeof(float));
+        indexBuffer =  std::make_unique<IndexBuffer>(indices, 2 * 3);
         
         VertexBufferLayout layout;
         layout.Push<float>(2);
@@ -54,19 +100,13 @@ public:
         
         shader = std::make_unique<Shader>();
         shader->Push(GL_VERTEX_SHADER, "assets/shaders/Basic.vert");
-        shader->Push(GL_FRAGMENT_SHADER, "assets/shaders/CircleSolid.frag");
+        shader->Push(GL_FRAGMENT_SHADER, "assets/shaders/CircleInRect.frag");
         shader->Compile();
         shader->Bind();
         shader->SetUniformVec4("u_Color", 1.0f, 1.0f, 1.0f, 1.0f);
-        shader->SetUniformMat4("u_MVP", m_MVP);
-    }
-    ~Circle() { }
-
-    void SetCentre(glm::vec2 position) {
-        m_Centre = position;
-        m_Model = glm::translate(glm::mat4(1.0f), glm::vec3(position, 0.0f));
-        m_MVP = m_Proj * m_View * m_Model;
-        shader->SetUniformMat4("u_MVP", m_MVP);
+        shader->SetUniform("u_CullRadius", 0.5f);
+        shader->SetUniform("u_EdgeSmooth", 1.2f/m_Radius);
+        shader->UnBind();
     }
    
 };
