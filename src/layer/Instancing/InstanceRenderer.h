7fc3ee9fc3a2f1adf0225350b70fe66b647090de
FarrukhKamal48 <farrukhkamal429@gmail.com>
2024-03-30 15:41:32 +0500

    changed VertexBuffer names in Instancing::Renderer

--- src/layer/Instancing/InstanceRenderer.h
+++ src/layer/Instancing/InstanceRenderer.h
@@ -19,12 +19,11 @@ public:
 private:
     std::unique_ptr<VertexArray> m_VertexArray;
     std::unique_ptr<IndexBuffer> m_IndexBuffer;
-    std::unique_ptr<VertexBuffer> m_VertexBuffer;
-    std::unique_ptr<VertexBuffer> m_TransBuffer;
+    std::unique_ptr<VertexBuffer> m_MeshBuffer;
+    std::unique_ptr<VertexBuffer> m_InstanceBuffer;
     glm::mat4 m_Proj;
     glm::mat4 m_View;
     
-    float m_Length = 10;
     void* m_Data;
     unsigned int m_DataSize;
     unsigned int m_InstaceCount;
@@ -36,15 +35,15 @@ public:
         Render::BasicBlend();
 
         m_VertexArray =  std::make_unique<VertexArray>();
-        m_VertexBuffer = std::make_unique<VertexBuffer>(Quad::GetVerticies(), Quad::SizeofVerticies());
-        m_TransBuffer = std::make_unique<VertexBuffer>(nullptr, m_DataSize, GL_DYNAMIC_DRAW);
+        m_MeshBuffer = std::make_unique<VertexBuffer>(Quad::GetVerticies(), Quad::SizeofVerticies());
+        m_InstanceBuffer = std::make_unique<VertexBuffer>(nullptr, m_DataSize, GL_DYNAMIC_DRAW);
         m_IndexBuffer =  std::make_unique<IndexBuffer>(Quad::GetIndicies(), 6);
         
-        m_VertexArray->AddBuffer(*m_VertexBuffer, Quad::Layout());
+        m_VertexArray->AddBuffer(*m_MeshBuffer, Quad::Layout());
 
         VertexBufferLayout translateLayout;
         translateLayout.Push<float>(2, 1);
-        m_VertexArray->AddBuffer(*m_TransBuffer, translateLayout);
+        m_VertexArray->AddBuffer(*m_InstanceBuffer, translateLayout);
 
         m_Proj = glm::ortho(0.0f, WIDTH, 0.0f, HEIGHT, -1.0f, 1.0f);
         m_View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
@@ -63,7 +62,7 @@ public:
 
     void Draw() {
         Render::Clear(1, 1, 1, 1);
-        m_TransBuffer->SetData(m_Data, m_DataSize);
+        m_InstanceBuffer->SetData(m_Data, m_DataSize);
         Render::DrawInstanced(*m_VertexArray, *m_IndexBuffer, *InstanceShader, m_InstaceCount);
     }
 };
