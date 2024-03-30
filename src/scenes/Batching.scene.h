a1233dacd7c509fc2d56f380eac3f6d778effb6f
FarrukhKamal48 <farrukhkamal429@gmail.com>
2024-03-30 06:58:35 +0500

    Moved BatchRenderer, Batch, Mesh, Quad and Vertex header files into a namespace 'Batching'

--- src/scenes/Batching.scene.h
+++ src/scenes/Batching.scene.h
@@ -1,21 +1,21 @@
 #pragma once
 #include "Scene.h"
-#include "../layer/BatchRenderer.h"
-#include "../meshes/Quad.h"
+#include "../layer/Batching/BatchRenderer.h"
+#include "../layer/Batching/Quad.h"
 
 #include "../vendor/glm/ext/matrix_transform.hpp"
 #include "../vendor/glm/ext/matrix_clip_space.hpp"
 
 namespace Scene {
 
-class Batching : public Scene {
+class Batcher : public Scene {
 private:
     static const int m_ObjCount = 100;
-    Mesh::Quad m_Shapes[m_ObjCount];
-    BatchRenderer<Mesh::Quad, 100> m_BatchRenderer;
+    Batching::Quad m_Shapes[m_ObjCount];
+    Batching::BatchRenderer<Batching::Quad, 100> m_BatchRenderer;
     
 public:
-    Batching() : 
+    Batcher() : 
         m_BatchRenderer(m_Shapes, m_ObjCount, glm::ortho(0.0f, WIDTH, 0.0f, HEIGHT, -1.0f, 1.0f), 
                         glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f)))
     {
@@ -26,7 +26,7 @@ public:
         m_BatchRenderer.BatchShader->SetUniform("u_EdgeSmooth", 1.2f/m_Shapes[0].GetScale().x);
         m_BatchRenderer.BatchShader->SetUniform("u_CullRadius", 0.5f);
     }
-    ~Batching() { }
+    ~Batcher() { }
 
     void Start() override {
         for (int i=0; i<m_ObjCount; i++) {
