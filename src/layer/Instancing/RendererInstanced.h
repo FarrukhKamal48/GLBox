6562dc8a22bced249be6be4af592ca9f061146b7
FarrukhKamal48 <farrukhkamal429@gmail.com>
2024-03-31 14:17:15 +0500

    Instance Renderer is fully templated and general

--- src/layer/Instancing/RendererInstanced.h
+++ src/layer/Instancing/RendererInstanced.h
@@ -9,10 +9,8 @@
 #include "VertexTypes.h"
 #include "MeshData.h"
 
-template<typename MeshType, typename InstanceDataType> class RendererInstanced { };
-
-template<typename InstanceDataType>
-class RendererInstanced<QuadData, InstanceDataType> {
+template<MeshType MeshT, VertexType VertexT, int InstanceCount>
+class RendererInstanced {
 public:
     std::unique_ptr<Shader> InstanceShader;
 private:
@@ -25,21 +23,20 @@ private:
     
     void* m_Data;
     unsigned int m_DataSize;
-    unsigned int m_InstaceCount;
     
 public:
-    RendererInstanced(void* data, unsigned int count) 
-        : m_Data(data), m_DataSize(count * sizeof(InstanceDataType)), m_InstaceCount(count)
+    RendererInstanced(void* data) 
+        : m_Data(data), m_DataSize(InstanceCount * sizeof(VertexT))
     {
         Render::BasicBlend();
 
         m_VertexArray =  std::make_unique<VertexArray>();
-        m_MeshBuffer = std::make_unique<VertexBuffer>(QuadData::GetVerticies(), QuadData::SizeofVerticies());
+        m_MeshBuffer = std::make_unique<VertexBuffer>(MeshLookup::GetVerticies(MeshT), MeshLookup::SizeofVerticies(MeshT));
+        m_IndexBuffer =  std::make_unique<IndexBuffer>(MeshLookup::GetIndicies(MeshT), MeshLookup::CountofIndicies(MeshT));
         m_InstanceBuffer = std::make_unique<VertexBuffer>(nullptr, m_DataSize, GL_DYNAMIC_DRAW);
-        m_IndexBuffer =  std::make_unique<IndexBuffer>(QuadData::GetIndicies(), QuadData::CountofIndicies());
         
-        m_VertexArray->AddBuffer(*m_MeshBuffer, QuadData::Layout());
-        m_VertexArray->AddBuffer(*m_InstanceBuffer, InstanceDataType::Layout(1));
+        m_VertexArray->AddBuffer(*m_MeshBuffer, MeshLookup::Layout(MeshT));
+        m_VertexArray->AddBuffer(*m_InstanceBuffer, VertexLookup::Layout(VertexT, 1));
 
         m_Proj = glm::ortho(0.0f, WIDTH, 0.0f, HEIGHT, -1.0f, 1.0f);
         m_View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
@@ -59,6 +56,6 @@ public:
     void Draw() {
         Render::Clear(1, 1, 1, 1);
         m_InstanceBuffer->SetData(m_Data, m_DataSize);
-        Render::DrawInstanced(*m_VertexArray, *m_IndexBuffer, *InstanceShader, m_InstaceCount);
+        Render::DrawInstanced(*m_VertexArray, *m_IndexBuffer, *InstanceShader, InstanceCount);
     }
 };
