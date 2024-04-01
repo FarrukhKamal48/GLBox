e81eecd75852a5fc878498aba873f3480fdb07f4
FarrukhKamal48 <farrukhkamal429@gmail.com>
2024-04-01 15:01:35 +0500

    made the obj count variable in Instancer.scene.h a preprocessor definition, which somehow increased the max objcount ( to over 1 million) so that doesn't lead tto segfault

--- src/scenes/Instancing.scene.h
+++ src/scenes/Instancing.scene.h
@@ -2,20 +2,20 @@
 
 #include "Scene.h"
 #include "../layer/Instancing/RendererInstanced.h"
+#define OBJCOUNT (int)1009999
 
 namespace Scene {
 
 class Instancer : public Scene {
 private:
-    static const int m_ObjCount = 521000;
     constexpr static const float m_MoveSpeed = 1000;
     constexpr static const float m_Bounciness = 0.9;
-    RendererInstanced<MeshType::Quad, VertexType::Pos2D, m_ObjCount> m_Renderer;
-    Pos2D m_Tranlations[m_ObjCount];
-    glm::vec2 m_Inc[m_ObjCount];
+    RendererInstanced<MeshType::Quad, VertexType::Pos2D, OBJCOUNT> m_Renderer;
+    Pos2D m_Tranlations[OBJCOUNT];
+    float m_Inc;
     
 public:
-    Instancer() : m_Renderer(&m_Tranlations) { 
+    Instancer() : m_Renderer(&m_Tranlations[0]) { 
         m_Renderer.ShaderInit("assets/shaders/instancing/Basic.vert", 
                               "assets/shaders/instancing/CircleInRect.frag");
         m_Renderer.InstanceShader->SetUniform("u_CullRadius", 0.5f);
@@ -23,36 +23,31 @@ public:
     ~Instancer() { }
 
     void Start() override {
-        for (int i = 0; i < m_ObjCount; i++) {
+        for (int i = 0; i < OBJCOUNT; i++) {
             m_Tranlations[i].position = glm::vec2(-WIDTH/2 + 5, 0);
-            m_Inc[i].x = (i+1.0f)/m_ObjCount * m_MoveSpeed;
-            m_Inc[i].y = (i+1.0f)/m_ObjCount * m_MoveSpeed;
         }
     }
 
     void Update(float dt) override {
-        for (int i = 0; i < m_ObjCount; i++) {
+        for (int i = 0; i < OBJCOUNT; i++) {
+            m_Inc = (i+1.0f)/OBJCOUNT * m_MoveSpeed;
             float& posX = m_Tranlations[i].position.x;
             if (posX > WIDTH-5) {
-                // m_Inc[i].x *= -m_Bounciness;
                 posX = 5;
             }
             else if (posX < 5) {
-                // m_Inc[i].x *= -m_Bounciness;
                 posX = WIDTH-5;
             }
-            posX += m_Inc[i].x * dt;
+            posX += m_Inc * dt;
             
             float& posY = m_Tranlations[i].position.y;
             if (posY > HEIGHT-5) {
-                // m_Inc[i].y *= -m_Bounciness;
                 posY = 5;
             }
             else if (posY < 5) {
-                // m_Inc[i].y *= -m_Bounciness;
                 posY = HEIGHT-5;
             }
-            posY += m_Inc[i].y * dt;
+            posY += m_Inc * dt;
         }
     }
     
