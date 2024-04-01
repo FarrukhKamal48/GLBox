444120c08c7a425aeee66131d8cc991434bea481
FarrukhKamal48 <farrukhkamal429@gmail.com>
2024-04-01 14:07:03 +0500

    added new MeshType::Triangle

--- src/layer/Instancing/MeshData.h
+++ src/layer/Instancing/MeshData.h
@@ -1,29 +1,51 @@
 #pragma once
 #include "../VertexArray.h"
 
-enum class MeshType { Quad };
+enum class MeshType { 
+    Triangle,
+    Quad,
+};
 
-class QuadData {
-private:
-    static constexpr float positions[16] = {
+class TriangleData {
+public:
+    TriangleData() = delete;
+    
+    static constexpr float Verticies[16] = {
         -5.0f, -5.0f, 0.0f, 0.0f,
          5.0f, -5.0f, 1.0f, 0.0f,
          5.0f,  5.0f, 1.0f, 1.0f,
         -5.0f,  5.0f, 0.0f, 1.0f,
     };
-    static constexpr unsigned int indices[6] = {
+    static constexpr unsigned int Indicies[6] = {
         0, 1, 2, 
         0, 2, 3
     };
 
 public:
+    static const VertexBufferLayout Layout() { 
+        VertexBufferLayout MeshLayout;
+        MeshLayout.Push<float>(2);
+        MeshLayout.Push<float>(2);
+        return MeshLayout; 
+    }
+    
+};
+class QuadData {
+public:
     QuadData() = delete;
     
-    static const float* GetVerticies() { return positions; }
-    static const unsigned int SizeofVerticies() { return sizeof(positions); }
-    static const unsigned int* GetIndicies() { return indices; }
-    static const unsigned int CountofIndicies() { return 6; }
+    static constexpr float Verticies[16] = {
+        -5.0f, -5.0f, 0.0f, 0.0f,
+         5.0f, -5.0f, 1.0f, 0.0f,
+         5.0f,  5.0f, 1.0f, 1.0f,
+        -5.0f,  5.0f, 0.0f, 1.0f,
+    };
+    static constexpr unsigned int Indicies[6] = {
+        0, 1, 2, 
+        0, 2, 3
+    };
 
+public:
     static const VertexBufferLayout Layout() { 
         VertexBufferLayout MeshLayout;
         MeshLayout.Push<float>(2);
@@ -39,35 +61,37 @@ public:
 
     static const float* GetVerticies(MeshType type) { 
         switch (type) {
-            case MeshType::Quad: return QuadData::GetVerticies();
-            default: return nullptr;
+            case MeshType::Triangle: return TriangleData::Verticies;
+            case MeshType::Quad    : return QuadData::Verticies;
+            default                : return nullptr;
         }
     }
     static const unsigned int SizeofVerticies(MeshType type) {
         switch (type) {
-            case MeshType::Quad: return QuadData::SizeofVerticies();
-            default: return 0;
+            case MeshType::Triangle: return sizeof(TriangleData::Verticies);
+            case MeshType::Quad    : return sizeof(QuadData::Verticies);
+            default                : return 0;
         }
     }
     static const unsigned int* GetIndicies(MeshType type) {
         switch (type) {
-            case MeshType::Quad: return QuadData::GetIndicies();
-            default: return nullptr;
+            case MeshType::Triangle: return TriangleData::Indicies;
+            case MeshType::Quad    : return QuadData::Indicies;
+            default                : return nullptr;
         }
     }
     static const unsigned int CountofIndicies(MeshType type) {
         switch (type) {
-            case MeshType::Quad: return QuadData::CountofIndicies();
-            default: return 0;
+            case MeshType::Triangle: return sizeof(TriangleData::Indicies);
+            case MeshType::Quad    : return sizeof(QuadData::Indicies);
+            default                : return 0;
         }
     }
     static const VertexBufferLayout Layout(MeshType type) { 
         switch (type) {
-            case MeshType::Quad: return QuadData::Layout();
-            default: {
-                VertexBufferLayout layout; 
-                return layout;
-            }
+            case MeshType::Triangle: return TriangleData::Layout();
+            case MeshType::Quad    : return QuadData::Layout();
+            default                : { VertexBufferLayout layout; return layout; }
         }
     }
     
