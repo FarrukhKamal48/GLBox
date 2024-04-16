5bfa5263fff8e76563aeb9139128ba4f73a5535c
FarrukhKamal48 <farrukhkamal429@gmail.com>
2024-04-16 14:26:55 +0500

    added a Struct SimData which contains all the data about the simulation

--- src/scenes/VerletInstancing.scene.h
+++ src/scenes/VerletInstancing.scene.h
@@ -85,20 +85,30 @@ public:
     }
 };
 
+struct SimData
+{
+    unsigned int EnabledCount = 1;
+    float SpawnRate = 100;
+    float SpawnTimer = 0;
+    float SpawnAngleRate = 2 * PI;
+    float SpawnAngle = PI/2;
+    float SpawnAngleDisplacement = 0;
+    glm::vec2 Gravity = {0, -1000};
+};
+
 
 namespace Scene {
 class VerletInstanced : public Scene {
 private:
-    constexpr static int m_ObjCount = 100000;
-    float m_SpawnRate = m_ObjCount;
+    constexpr static int m_ObjCount = 1000;
     Pos_Scale_Col* m_ObjData = new Pos_Scale_Col[m_ObjCount+1];
     RigidBody* m_Bodies = new RigidBody[m_ObjCount];
     Constraint m_Constraint;
-    glm::vec2 m_Gravity;
+    SimData m_SimData;
     RendererInstanced<QuadData, Pos_Scale_Col, m_ObjCount+1> m_Renderer;
 public:
     VerletInstanced() 
-        : m_Constraint({0, HEIGHT}, {WIDTH, 0}), m_Gravity({0, -1000}), m_Renderer(m_ObjData) 
+        : m_Constraint({0, HEIGHT}, {WIDTH, 0}), m_Renderer(m_ObjData) 
     {
         m_Renderer.ShaderInit("assets/shaders/instancing/BasicColorScale.vert", 
                               "assets/shaders/instancing/CircleInRectColor.frag");
@@ -120,26 +130,25 @@ public:
             m_Bodies[i].pos = &m_ObjData[i+1].position;
             m_Bodies[i].pos_old = *m_Bodies[i].pos;
             m_Bodies[i].bouncines = 1.0f-p;
-            // m_Bodies[i].velocity(glm::vec2(glm::cos(p * 2 * glm::pi<float>()), glm::sin(p * 2 * glm::pi<float>())));
-            m_Bodies[i].velocity({-5, 10});
+            float theta = m_SimData.SpawnAngleDisplacement + m_SimData.SpawnAngle/2 * (sin(p * m_SimData.SpawnAngleRate) - 1);
+            m_Bodies[i].velocity(10.0f * glm::vec2(cos(theta), sin(theta)));
         }
         m_ObjData[0].position = glm::vec2(WIDTH/2, HEIGHT/2);
         m_ObjData[0].scale = glm::vec2(HEIGHT/2);
         m_ObjData[0].color = glm::vec4(0,0,0,1);
-        m_SpawnRate = m_ObjCount;
+        
+        m_SimData.SpawnRate = m_ObjCount;
     }
 
-    int enabledCount = 0; 
-    float spawnTimer = 0;
     void Update(float dt) override {
-        spawnTimer += dt;
-        if (spawnTimer >= 1.0f/m_SpawnRate && enabledCount < m_ObjCount) {
-            spawnTimer = 0;
-            enabledCount++;
+        m_SimData.SpawnTimer += dt;
+        if (m_SimData.SpawnTimer >= 1.0f/m_SimData.SpawnRate && m_SimData.EnabledCount < m_ObjCount) {
+            m_SimData.SpawnTimer = 0;
+            m_SimData.EnabledCount++;
         }
-        for (int i = 0; i < enabledCount; i++) {
+        for (int i = 0; i < m_SimData.EnabledCount; i++) {
             RigidBody& body = m_Bodies[i];
-            body.accelerate(m_Gravity);
+            body.accelerate(m_SimData.Gravity);
             body.updatePosition(dt);
             m_Constraint.ApplyCircle(body, m_ObjData[i+1].scale);
         }
