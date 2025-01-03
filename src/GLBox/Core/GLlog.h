18ccc309cbef5593ed555dd28da21eed76545c91
FarrukhKamal48 <farrukhkamal429@gmail.com>
2025-01-03 16:20:31 +0500

    Added a Basic logging function which simply outputs each value in a row seperated by commas:

--- src/GLBox/Core/GLlog.h
+++ src/GLBox/Core/GLlog.h
@@ -1,6 +1,17 @@
 #pragma once
+#include <glbpch.h>
 
 #define GLCall(X) GLClearError(); X; GLLogCall(#X, __FILE__, __LINE__)
 
 void GLClearError();
 bool GLLogCall(const char* funcion, const char* file, int line);
+
+#define BASIC_LOG(...) LogList({__VA_ARGS__})
+
+template<typename T>
+void LogList(std::initializer_list<T> list) {
+    for (T x : list) {
+        std::cout << x << ", ";
+    }
+    std::cout << '\n';
+}
